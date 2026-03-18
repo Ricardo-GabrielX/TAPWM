bd780ee0452708aaaff60916a568f97dbafd9dec
Gabriel <gabrieloliveira280807@gmail.com>
2026-03-18 18:08:47 -0300

    add: atividade 2

---
+++ Atividade2/PEmpregado/PEmpregado/FrmHorista.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PEmpregado
+{
+    public partial class FrmHorista: Form
+    {
+        public FrmHorista()
+        {
+            InitializeComponent();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnInstanciarHorista_Click(object sender, EventArgs e)
+        {
+            Horista objHorista = new Horista();
+            objHorista.Matricula = Convert.ToInt32(txtMatricula.Text);
+            objHorista.NomeEmpregado = (txtNome.Text);
+            objHorista.DataEntradaEmpresa = Convert.ToDateTime(txtData.Text);                                                    
+            objHorista.DiasFalta = Convert.ToInt32(txtDiasFalta.Text);
+            objHorista.NumeroHora = Convert.ToInt32(txtNumeroHora.Text);
+
+            MessageBox.Show("Nome=" + objHorista.NomeEmpregado +
+                "\n" + "Matrícula=" + objHorista.Matricula +
+                "\n" + "Tempo Trabalho= " + objHorista.TempoTrabalho() +
+                "\n" + "Data Entrada Empresa= " + objHorista.DataEntradaEmpresa.ToShortTimeString() +
+                "\n" + "Salário hora= " + objHorista.SalarioHora +
+                "\n" + "Número hora= " + objHorista.NumeroHora +
+                "\n" + "Dias Falta= " + objHorista.DiasFalta +
+                "\n" + "Salário Bruto= " + objHorista.SalarioBruto().ToString("N2") +
+                "\n" + "Tempo Trabalho= " + objHorista.TempoTrabalho());
+
+                
+
+        }
+
+        private void txtNumeroHora_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnInstanciarHoristaParametros_Click(object sender, EventArgs e)
+        {
+            Horista objMensalista = new Horista();
+            //Convert.ToInt16(txtData.Text);
+            objMensalista.NomeEmpregado = txtNome.Text;
+            objMensalista.Matricula = Convert.ToInt32(txtMatricula.Text);
+            objMensalista.DataEntradaEmpresa = Convert.ToDateTime(txtData.Text);
+
+            objMensalista.SalarioHora = Convert.ToInt32(txtSalario.Text);
+
+            MessageBox.Show("Nome=" + objMensalista.NomeEmpregado +
+               "\n" + "Matrícula=" + objMensalista.Matricula +
+               "\n" + "Tempo Trabalho= " + objMensalista.TempoTrabalho() +
+               "\n" + "Salário Final= " + objMensalista.SalarioBruto().ToString("N2"));
+        }
+    }
+}
---
+++ Atividade2/PEmpregado/PEmpregado/FrmMensalista.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PEmpregado
+{
+    public partial class FrmMensalista: Form
+    {
+        public FrmMensalista()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInstanciarMensalitsta_Click(object sender, EventArgs e)
+        {
+            Mensalista objMensalista = new Mensalista();
+
+            objMensalista.NomeEmpregado = txtNome.Text;
+            objMensalista.Matricula = Convert.ToInt32(matricula.Text);
+            objMensalista.DataEntradaEmpresa = Convert.ToDateTime(txtData.Text);
+
+            objMensalista.SalarioMensal = Convert.ToDouble(txtSalario.Text);
+
+            MessageBox.Show("Nome=" + objMensalista.NomeEmpregado +
+                "\n" + "Matrícula=" + objMensalista.Matricula +
+                "\n" + "Tempo Trabalho= " + objMensalista.TempoTrabalho() +
+                "\n" + "Salário Final= " + objMensalista.SalarioBruto().ToString("N2"));
+        }
+
+        private void txtData_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtSalario_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNome_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void matricula_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnInstanciarMensalistaParametros_Click(object sender, EventArgs e)
+        {
+            Mensalista objMensalista = new Mensalista();
+                //Convert.ToInt16(txtData.Text);
+            objMensalista.NomeEmpregado = txtNome.Text;
+            objMensalista.Matricula = Convert.ToInt32(matricula.Text);
+            objMensalista.DataEntradaEmpresa = Convert.ToDateTime(txtData.Text);
+
+            objMensalista.SalarioMensal = Convert.ToDouble(txtSalario.Text);
+
+            MessageBox.Show("Nome=" + objMensalista.NomeEmpregado +
+               "\n" + "Matrícula=" + objMensalista.Matricula +
+               "\n" + "Tempo Trabalho= " + objMensalista.TempoTrabalho() +
+               "\n" + "Salário Final= " + objMensalista.SalarioBruto().ToString("N2"));
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FrmMensalista_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
