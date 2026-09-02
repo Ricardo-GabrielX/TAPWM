125a30d2c04269c091666e7fe220cdc5a7fbe313
ricardo-gabrielx <gabrieloliveira280807@gmail.com>
2026-09-02 13:50:22 -0300

    feat: adicionar atividade 19, corrigir atividade 18 e configurar .gitignore

--- Atividade18/exercicios/app.js
+++ Atividade18/exercicios/app.js
@@ -7,21 +7,18 @@ app.get('/', function(req, res) {
     res.send('Hello, World!');
 });
 
-
-
-app.get('secao/historia', function(req, res) {
-    res.render("<h1>História da Fatec</h1><p>Esta é a página sobre a história da Fatec Sorocaba.</p>");
+app.get('/secao/historia', function(req, res) {
+    res.render('secao/historia');
 });
 
 app.get('/secao/cursos', function(req, res) {
-    res.render("<h1>Cursos da Fatec</h1><p>Esta é a página sobre os cursos oferecidos pela Fatec Sorocaba.</p>");
+    res.render('secao/cursos');
 });
 
 app.get('/secao/professores', function(req, res) {
-    res.render("<h1>Professores da Fatec</h1><p>Esta é a página sobre os professores da Fatec Sorocaba.</p>");
+    res.render('secao/professores');
 });
 
 app.listen(3000, function() {
     console.log('Servidor rodando na porta 3000');
 });
-
