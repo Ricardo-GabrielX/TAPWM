c9d38d41be72631117ceff5de3318de3ab069e16
ricardo-gabrielx <gabrieloliveira280807@gmail.com>
2026-09-16 14:15:59 -0300

    feat: atualza exercicios 19

--- Atividade19/exercicios/app/routes/professores.js
+++ Atividade19/exercicios/app/routes/professores.js
@@ -1,5 +1,33 @@
-module.exports = function(app) {
-    app.get('/informacao/professores', function(req, res) {
-        res.render('informacao/professores');
-    });
+module.exports = function(app){
+   app.get('/informacao/professores', function(req,res){
+       const sql = require ('mssql/msnodesqlv8');
+ 
+       const sqlConfig = {
+           user: 'xx',
+           password: 'xxxx',
+           database: 'BD', 
+           driver: 'msnodesqlv8',
+           options: {
+               encrypt: false,
+               trustServerCertificate: true,
+           }
+       }
+  
+      async function getProfessores() {
+          try {
+              const pool = await sql.connect(sqlConfig);
+          
+               const results = await pool.request().query('SELECT * from PROFESSORES')
+          
+               res.json(results.recordset);
+ 
+              res.render('informacao/professores',{profs: results.recordset})
+    
+           } catch (err) {
+               console.log(err)
+          }
+        
+       }
+      getProfessores();
+   });
 }
