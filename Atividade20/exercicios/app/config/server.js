e918664857d975c8cf8a455768fbf449bb28a5c0
ricardo-gabrielx <gabrieloliveira280807@gmail.com>
2026-09-23 13:48:11 -0300

    feat: adiciona lib consign, e altera para lidar com ela

--- Atividade20/exercicios/app/config/server.js
+++ Atividade20/exercicios/app/config/server.js
@@ -1,6 +1,9 @@
 let express = require('express');
+let consign = require('consign');
+
 let app = express(); // executando o express
 app.set('view engine', 'ejs'); // o mecanismo de engine a ser usado
 
 app.set('views','./app/views'); // diretório onde os arquivos de visualização (views) estão localizados
-module.exports = app;
+consign().include('app/routes').into(app); // inclui os arquivos de rotas no app
+module.exports = app;   
